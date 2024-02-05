0ba7b1de4f04f12b5e3c7197e7d77106079d70af
hilkiano <hilkia.nohansen@gmail.com>
2024-02-05 22:41:18 +0700

    add: meta description

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,6 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="description" content="Melayani Perawatan Estetika, Terbaik di Tasikmalaya.">
     @if (App::environment())
         <title>{{ config('app.name', 'Apotek') }}</title>
     @else
