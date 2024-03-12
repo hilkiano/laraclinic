2ed001ca18ff3b6e9812ffd56c92b52fde315632
hilkiano <hilkia.nohansen@gmail.com>
2024-03-12 15:30:26 +0700

    add softdelete transaction

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,10 +4,13 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Carbon;
 
 class Transaction extends Model
 {
+    use SoftDeletes;
+
     protected $table = 'transactions';
     protected $primaryKey = 'id';
     protected $keyType = 'string';
